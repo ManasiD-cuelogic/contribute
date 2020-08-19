e5101a1f8bc47c9d9f54ae3ae89fa83da38d3629
Manasi <Manasi@cue>
2020-08-19 15:29:07 +0530

    add new method add and subtract

---
+++ cal.py
@@ -0,0 +1,13 @@
+def add(a,b):
+    addition=a+b
+    print("Addition : ",addition)
+def subtract(a,b):
+    sub=a-b
+    print("Subtraction : ",sub)
+if __name__ == __"main"__:
+    print("Enter First Number")
+    x=int(input())
+    print("Enter Second NUmber")
+    y=int(input())
+    add(x,y)
+    subtract(x,y)
